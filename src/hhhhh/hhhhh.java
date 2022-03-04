7fe4406231d840d189c17d9e90dfca3370a6d713
Aure219 <1471665755@qq.com>
2022-03-04 12:52:15 +0800

    1

--- src/hhhhh/hhhhh.java
+++ src/hhhhh/hhhhh.java
@@ -1,10 +1,17 @@
 package hhhhh;
 
+import java.util.Scanner;
+
 public class hhhhh {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld!");
+	System.out.println("Hello!");
+//	Scanner in =new Scanner(System.in);
+//		System.out.println("echo"+in.nextLine());
+	System.out.println("2+3="+5);
+	System.out.println("2+3="+(2+3));
+	System.out.println("2+3="+2+3);
+	System.out.println(2+3+"=2+3="+5);
 	}
-
 }
